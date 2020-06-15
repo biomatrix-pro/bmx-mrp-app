254263f63c9d9586c8d84130b8c804653272ff9f
deksden <deksden@deksden.com>
2020-06-15 22:23:37 +0700

    feat: social login WIP

--- src/core/auth-provider.js
+++ src/core/auth-provider.js
@@ -4,22 +4,52 @@ import fetch, { Request, Headers } from 'node-fetch'
 import { ApiUrl } from './data-provider'
 
 const authProvider = {
-  login: ({ username, password }) => {
-    const request = new Request(`${ApiUrl}/auth/login`, {
-      method: 'POST',
-      body: JSON.stringify({ email: username, password }),
-      headers: new Headers({ 'Content-Type': 'application/json' })
-    })
-    return fetch(request)
-      .then(response => {
-        if (response.status < 200 || response.status >= 300) {
-          throw new Error(response.statusText)
-        }
-        return response.json()
+  login: ({ social, username, password, code, state }) => {
+    if (username && password) {
+      const request = new Request(`${ApiUrl}/auth/login`, {
+        method: 'POST',
+        body: JSON.stringify({ email: username, password }),
+        headers: new Headers({ 'Content-Type': 'application/json' })
       })
-      .then(({ token }) => {
-        window.localStorage.setItem('token', token)
+      return fetch(request)
+        .then(response => {
+          if (response.status < 200 || response.status >= 300) {
+            throw new Error(response.statusText)
+          }
+          return response.json()
+        })
+        .then(({ token }) => {
+          window.localStorage.setItem('token', token)
+        })
+    } else if (social && !username && !password && !code && !state) {
+      // 1 redirect to social login:
+      if (social !== 'yandex') {
+        // as for now - only Yandex login in supported:
+        throw new Error('Only Yandex is supported')
+      }
+      window.location =
+        `https://oauth.yandex.ru/authorize?response_type=code&client_id=${process.env.REACT_APP_SOCIAL_YANDEX_APP}` +
+        `&redirect_uri=${encodeURIComponent(process.env.REACT_APP_URL + '/#/login')}&state=app`
+    } else if (!username && !password && code && state) {
+      console.log('Code grabbed!')
+      console.log(code)
+      // login via server to get JWT with social login
+      const request = new Request(`${ApiUrl}/auth/social/yandex`, {
+        method: 'POST',
+        body: JSON.stringify({ code }),
+        headers: new Headers({ 'Content-Type': 'application/json' })
       })
+      return fetch(request)
+        .then(response => {
+          if (response.status < 200 || response.status >= 300) {
+            throw new Error(response.statusText)
+          }
+          return response.json()
+        })
+        .then(({ token }) => {
+          window.localStorage.setItem('token', token)
+        })
+    }
   },
   logout: () => {
     window.localStorage.removeItem('token')
--- src/forms/login-form.js
+++ src/forms/login-form.js
@@ -1,16 +1,71 @@
-import React, { useState } from 'react'
-import { Login, LoginForm } from 'react-admin'
+import React, { useState, useEffect } from 'react'
+import { Login, LoginForm, useLogin } from 'react-admin'
+import { useLocation } from 'react-router-dom'
 import Button from '@material-ui/core/Button'
 import CardActions from '@material-ui/core/CardActions'
 import Divider from '@material-ui/core/Divider'
+import { makeStyles } from '@material-ui/core/styles'
+import CircularProgress from '@material-ui/core/CircularProgress'
+import qs from 'qs'
 
-export const MyLoginPage = ({ classes, userLogin }) => {
+const useStyles = makeStyles(theme => ({
+  button: {
+    width: '100%'
+  },
+  icon: {
+    marginRight: theme.spacing(1)
+  }
+}))
+
+export const MyLoginPage = () => {
+  const [loading, setLoading] = useState(false)
+  const classes = useStyles()
+  const userLogin = useLogin()
+  const location = useLocation()
+
+  useEffect(() => {
+    // const { searchParams } = new URL(window.location.href)
+    const params = qs.parse(location.search, { ignoreQueryPrefix: true })
+
+    console.log(params)
+    const code = params.code
+    const state = params.state
+
+    // If code is present, we came back from the provider
+    if (code && state) {
+      setLoading(true)
+      userLogin({ code, state })
+    }
+  }, [userLogin])
+
+  const handleLogin = () => {
+    setLoading(true)
+    userLogin({ social: 'yandex' }) // Do not provide code, just trigger the redirection
+  }
+
   return (
     <Login backgroundImage='https://loremflickr.com/1024/768/computers'>
       <LoginForm />
-      <Divider variant="middle" />
+      <Divider variant='middle' />
       <CardActions>
-        <Button variant='contained' color='primary' fullWidth>Login with Yandex</Button>
+        <Button
+          className={classes.button}
+          variant='contained'
+          color='primary'
+          type='submit'
+          onClick={handleLogin}
+          disabled={loading}
+          fullWidth
+        >
+          {loading && (
+            <CircularProgress
+              className={classes.icon}
+              size={18}
+              thickness={2}
+            />
+          )}
+          Login with Yandex
+        </Button>
       </CardActions>
     </Login>
   )
